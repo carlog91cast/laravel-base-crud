60dd3cb64be99c336daf67361590d9d70e18a55c
bigeppo <carlog91cast@gmail.com>
2022-09-16 15:56:17 +0200

    add fillable in model Comic

--- app/Providers/Comic.php
+++ app/Providers/Comic.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
-
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'thumb',
+        'price',
+        'series',
+        'type',
+        'sale_date'
+    ];
 }
